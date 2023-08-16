e74233f616575acbc9ec9a382439877996165e1c
SaulihaSaeed <saulihasaeed@gmail.com>
2023-08-16 09:54:26 -0400

    jbj,bvuj

---
+++ github.swift
@@ -0,0 +1,20 @@
+//
+//  github.swift
+//  gitHub
+//
+//  Created by scholar on 8/16/23.
+//
+
+import SwiftUI
+
+struct github: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct github_Previews: PreviewProvider {
+    static var previews: some View {
+        github()
+    }
+}
